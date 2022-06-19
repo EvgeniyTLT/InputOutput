31dbd7251c72106ce61739518ec2e37351bba105
EvgeniyTLT <Toy444@ya.ru>
2022-06-19 22:51:35 +0300

    add

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("folder1/file1.txt");
+        System.out.println(file.getName());
+    }
+}
