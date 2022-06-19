f63abd0db42c39418f84c42e8bff04f35a8a90a5
EvgeniyTLT <Toy444@ya.ru>
2022-06-19 22:52:22 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         File file = new File("folder1/file1.txt");
         System.out.println(file.getName());
+        System.out.println(file.exists());
     }
 }
