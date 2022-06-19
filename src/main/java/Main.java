ecd5b2e6787fbe2e53f57d4d0680cdc5c1a2140d
EvgeniyTLT <Toy444@ya.ru>
2022-06-19 22:57:01 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        File directory = new File("folder1");
         File file = new File("folder1/file1.txt");
+        try {
+            directory.mkdir();
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println(file.getName());
         System.out.println(file.exists());
         System.out.println(file.isDirectory());
