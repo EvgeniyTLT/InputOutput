893803571b769ad6647f945cbf13ed99ba7a24a4
EvgeniyTLT <Toy444@ya.ru>
2022-06-20 16:08:01 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,23 @@
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        File directory = new File("folder/folder2/folder1");
-        File file = new File("folder/folder2/folder1/file1.txt");
-        File file2 = new File("folder/folder2/folder1/file2.txt");
-        File file3 = new File("folder/folder2/folder1/file3.txt");
+        File file = new File("1.txt");
         try {
-            directory.mkdirs();
             file.createNewFile();
-            file2.createNewFile();
-            file3.createNewFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        File[] files = directory.listFiles((dir, name) -> name.contains("1"));
-        for (File f: files
-             ) {
-            System.out.println(f.getName());
+
+        try (InputStream inputStream = new FileInputStream(file)) {
+            int a = inputStream.read();
+            while (a != -1) {
+                System.out.print((char) a);
+                a = inputStream.read();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
