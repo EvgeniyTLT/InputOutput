28ef38bcbc3e8ef79e17b4317acb3978d28e9743
EvgeniyTLT <Toy444@ya.ru>
2022-06-21 21:49:14 +0300

    add

--- src/main/java/Main4.java
+++ src/main/java/Main4.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main4 {
@@ -6,9 +6,33 @@ public class Main4 {
         File directory = new File("folder");
         File file = new File(directory, "names.txt");
         Scanner scanner = new Scanner(System.in);
+        String stop = "B";
+        String result = "";
+        while (stop.equals("B")) {
+            System.out.println("enter name: ");
+            String name = scanner.nextLine();
+            if (name.equals("exit")) {
+                stop = "a";
+                try (InputStream inputStream = new BufferedInputStream(new FileInputStream(file))) {
+                    int a = inputStream.read();
+                    StringBuilder result1 = new StringBuilder();
+                    while (a != -1) {
+                        result1.append((char) a);
+                        a = inputStream.read();
+                    }
+                    System.out.println(result1.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                try (OutputStream outputStream = new FileOutputStream(file, true)) {
+                    String names = name + " ";
+                    outputStream.write(names.getBytes());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println(a+b);
     }
 }
