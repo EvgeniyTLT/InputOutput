5cd1ad5411e2d2be06ded2f6077951eec5032049
EvgeniyTLT <Toy444@ya.ru>
2022-06-21 21:14:07 +0300

    add

--- src/main/java/Main3.java
+++ src/main/java/Main3.java
@@ -1,18 +1,28 @@
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 public class Main3 {
     public static void main(String[] args) throws IOException {
         File directory = new File("folder");
         File file = new File(directory, "names.txt");
-        directory.mkdir();
-        file.createNewFile();
-        try (OutputStream outputStream = new FileOutputStream(file)) {
-            String names = "John Max Nick Thomas Andrew Roman Herbert Alex";
-            outputStream.write(names.getBytes());
+
+//        try (OutputStream outputStream = new FileOutputStream(file, true)) {
+//            String names = "\nJohn Max Nick Thomas Andrew Roman Herbert Alex";
+//            outputStream.write(names.getBytes());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        try (InputStream inputStream = new BufferedInputStream(new FileInputStream(file))) {
+            long before = System.currentTimeMillis();
+            int a = inputStream.read();
+            StringBuilder result = new StringBuilder();
+            while (a != -1) {
+                result.append((char) a);
+                a = inputStream.read();
+            }
+            long after = System.currentTimeMillis();
+            System.out.println(after - before);
         } catch (Exception e) {
             e.printStackTrace();
         }
