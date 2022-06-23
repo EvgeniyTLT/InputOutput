9b8a091c8719b655738621fa10a1c70084a214ec
EvgeniyTLT <Toy444@ya.ru>
2022-06-23 10:11:56 +0300

    add

--- src/main/java/Main8.java
+++ src/main/java/Main8.java
@@ -3,17 +3,22 @@ import java.io.*;
 public class Main8 {
     public static void main(String[] args) {
         File file = new File("users.urs");
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        User user = new User("John", "Smith", 25);
-        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file))) {
-            objectOutputStream.writeObject(user);
+//        try {
+//            file.createNewFile();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        User user = new User("John", "Smith", 25);
+//        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file))) {
+//            objectOutputStream.writeObject(user);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))) {
+            User user = (User) objectInputStream.readObject();
+            System.out.println(user);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
