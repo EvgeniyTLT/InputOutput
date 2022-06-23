0aefa42bc7a8ce6f4fe6b202d96684d658f37e17
EvgeniyTLT <Toy444@ya.ru>
2022-06-23 10:09:12 +0300

    add

---
+++ src/main/java/Main8.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main8 {
+    public static void main(String[] args) {
+        File file = new File("users.urs");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        User user = new User("John", "Smith", 25);
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(file))) {
+            objectOutputStream.writeObject(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
